5a13c70df02617b4fde739e3663e54386b123544
Mayank <ffmayank097@gmail.com>
2025-11-19 14:03:16 +0530

    OOPS

---
+++ oops.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+
+// class User{
+
+
+// private:
+//     int id;
+//     string password;
+
+// public:
+//     string userName;
+
+
+
+//     User(int id){
+//         this->id = id;
+//     }
+
+
+//     //setter
+//     void setPassword(string password){
+//         this->password = password;
+//     }
+
+//     //getter
+//     string getPassword(){
+//         return password;
+//     }
+
+
+
+
+// };
+
+
+
+// class Car{
+
+// public:
+//     string name;
+//     string color;
+//     int *mileage;
+
+//     Car(string name , string color){
+//         this->name = name;
+//         this->color = color;
+//         mileage = new int;
+//         *mileage = 12;
+//     }
+
+//     Car(Car &original){
+//         this->name = original.name;
+//         this->color = original.color;
+//         mileage = new int;
+//         *mileage = *original.mileage;
+
+//     }
+// };
+
+
+// class Animal{
+//     public:
+//        string color;
+
+//     void eat(){
+//         cout<<"Eating........\n";
+//     }
+
+
+// };
+
+
+// class Fish : public Animal{
+//     public :
+//         int fins;
+
+//     void swim(){
+//         cout<<"Swimming.........\n";
+//     }
+
+// };
+
+// class Turtle : public Fish{
+//     public:
+//        int speed;
+
+//     void run(){
+//         cout<<"Runnninggg at 180kmph.....\n";
+
+//     }
+// };
+
+
+// class Print{
+
+// public:
+//     void show(int n){
+//         cout<<n<<"\n";
+//     }
+
+//     void show(string str){
+//         cout<<str<<"\n";
+//     }
+
+
+// };
+
+
+int main(){
+
+    // Print obj1;
+    // obj1.show(20);
+    // obj1.show("Mayank");
+
+
+    // Fish f1;
+    // f1.fins = 20;
+    // cout<<f1.fins<<endl;
+    // f1.eat();
+    // f1.swim();
+
+    // Turtle t1;
+    // t1.speed = 180;
+    // t1.eat();
+    // t1.run();
+    // t1.swim();
+    // cout<<t1.speed<<"\n";
+
+    // User user1(101);
+    // user1.userName = "Mayank";
+    // user1.setPassword("abcdef@123");
+
+    // cout<<user1.userName<<endl;
+    // cout<<user1.getPassword()<<endl;
+
+    // Car c1("Maruti Suzuki","white");
+    // Car c2(c1);
+
+    // cout<<*c2.mileage<<endl;
+    // *c2.mileage = 20;
+    // cout<<*c1.mileage<<endl;
+    // cout<<*c2.mileage<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
